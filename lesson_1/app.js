117fc4d56a6832d42e7817fd5f443734d4b688ce
Valery555 <sokolchenkov@gmail.com>
2022-06-09 20:00:45 +0300

    sort via call back, dynamic option & promises

---
+++ lesson_1/app.js
@@ -0,0 +1,23 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const sortFolder = async (read, gender, write) => {
+    try {
+        const files = await fs.readdir(path.join(__dirname, read));
+
+        for (const file of files) {
+            const redFolderPath = path.join(__dirname, read, file);
+            const data = await fs.readFile(redFolderPath);
+            const user = JSON.parse(data.toString());
+
+            if (user.gender === gender) {
+                await fs.rename(redFolderPath, path.join(__dirname, write, file));
+            }
+        }
+    }catch (e) {
+        console.error(e);
+    }
+}
+
+sortFolder('girls', 'male', 'boys');
+sortFolder('boys', 'female', 'girls');
