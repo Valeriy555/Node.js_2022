aa3bbffd4050b907d630f047f0b92c5c4f3c5e69
Valery555 <sokolchenkov@gmail.com>
2022-06-09 16:30:01 +0300

    sort via call back

--- lesson_1/index.js
+++ lesson_1/index.js
@@ -1,17 +1,55 @@
-const fs = require ('fs');
+const fs = require('fs');
 const path = require('path')
 
 const sortBoysFolder = () => {
 
-fs.readdir('./boys',(err,files) => {
-    if (err) return console.log(err);
-
-    files.forEach((file) => {
-        fs.readFile(path.join(__dirname, 'boys', file),(err, data) => {})
+    fs.readdir('./boys', (err, files) => {
         if (err) return console.log(err);
-    })
-})
 
+        files.forEach((file) => {
+            const redFolderPath = path.join(__dirname, 'boys', file)
+
+            fs.readFile(redFolderPath, (err, data) => {
+
+                if (err) return console.log(err);
+
+                const user = JSON.parse(data.toString());
+
+                if (user.gender === 'female') {
+                    fs.rename(redFolderPath, path.join(__dirname, 'girls', file), (err) => {
+                        if (err) return console.log(err);
+                    });
+                }
+            });
+        });
+    });
 }
 
 sortBoysFolder();
+
+
+const sortGirlsFolder = () => {
+
+    fs.readdir('./girls', (err, files) => {
+        if (err) return console.log(err);
+
+        files.forEach((file) => {
+            const redFolderPath = path.join(__dirname, 'girls', file)
+
+            fs.readFile(redFolderPath, (err, data) => {
+
+                if (err) return console.log(err);
+
+                const user = JSON.parse(data.toString());
+
+                if (user.gender === 'male') {
+                    fs.rename(redFolderPath, path.join(__dirname, 'boys', file), (err) => {
+                        if (err) return console.log(err);
+                    });
+                }
+            });
+        });
+    });
+}
+
+sortGirlsFolder();
