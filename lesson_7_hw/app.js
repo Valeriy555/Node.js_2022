8b9ac39957aad47609e6c67ad0d47207d3bbf08e
Valery555 <sokolchenkov@gmail.com>
2022-07-01 02:33:23 +0300

    lesson7_hw

--- lesson_7_hw/app.js
+++ lesson_7_hw/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config('path');
+const path = require('path');
 require('dotenv').config({ path: path.join(process.cwd(), 'environments', `${process.env.MODE}.env`)})
 
 const {userRouter, authRouter} = require("./routes");
